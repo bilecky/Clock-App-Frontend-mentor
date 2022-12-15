d371bf2aed214fab4c9180a33b8f313418edaf9f
bilecky <bilecky95@gmail.com>
2022-12-15 01:51:07 +0100

    ft: Quotes api added

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ const clock = document.querySelector('.time')
 const localization = document.querySelector('.localization')
 const message = document.querySelector('.weather-message')
 const timezone = document.querySelector('.abbreviation')
+const quote = document.querySelector('.quote')
+const btn = document.querySelector('.button')
+const author = document.querySelector('.author')
+
 const getTime = () => {
 	const URL = 'http://worldtimeapi.org/api/ip'
 
@@ -16,5 +20,20 @@ const getTime = () => {
 			timezone.textContent = abbreviation
 		})
 }
+
+const getQuote = () => {
+	const URL = 'https://type.fit/api/quotes'
+
+	fetch(URL)
+		.then(response => response.json())
+		.then(data => {
+			const random = Math.floor(Math.random() * data.length)
+			quote.textContent = data[random].text
+			author.textContent = data[random].author
+			console.log(data[random])
+		})
+}
+
+getQuote()
 getTime()
 setInterval(getTime, 10000)
